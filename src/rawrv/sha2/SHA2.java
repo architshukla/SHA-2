02a722e8f9ff66c0d13aee0eb6286c3f1ef31ab3
Archit Shukla <architshukla2@gmail.com>
2013-11-13 01:32:17 +0530

    Added SHA2.java and Test.java

---
+++ src/rawrv/sha2/SHA2.java
@@ -0,0 +1,9 @@
+package rawrv.sha2;
+
+public final class SHA2 {
+	
+	public static String hash() {
+		// TODO Implement main hash function
+		return null;
+	}
+}
---
+++ src/rawrv/sha2/Test.java
@@ -0,0 +1,8 @@
+package rawrv.sha2;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println(SHA2.hash());
+	}
+}
